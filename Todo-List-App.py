f2297fb8d499a8f3eac3f3576836d4e7b1dc3592
Eliel10268 <ndaeliel123@gmail.com>
2023-01-11 19:56:03 +0000

    First Day of Python Todo List App

---
+++ Todo-List-App.py
@@ -0,0 +1,13 @@
+
+user_input = "Enter a Todo : "
+
+
+todo1 = input(user_input)
+todo2 = input(user_input)
+todo3 = input(user_input)
+
+todos = [todo1, todo2, todo3]
+
+print((todos))
+
+print(type(todo1))
